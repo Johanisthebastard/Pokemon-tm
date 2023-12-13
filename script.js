7efe0b882f5c56567ddbfeffeee7a8508ed33baf
Johan Mattsson <mattsson.johan.93@gmail.com>
2023-12-13 10:09:31 +0100

    api funkar

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+
 	const pokemonFinderLink = document.getElementById('pokemonFinderLink');
 	const myTeamLink = document.getElementById('myTeamLink');
 	const mainImage = document.getElementById('mainImage');
@@ -23,3 +24,49 @@ document.addEventListener('DOMContentLoaded', function () {
 	  pokemonFinderBox.style.display = 'none';
 	});
   });
+
+//Hämta pokemon från API
+
+const pokedex = document.getElementById("pokedex");
+
+console.log(pokedex);
+
+const fetchPokemon = () => {
+	const promises = [];
+	for( let i = 1; i <= 5; i++ ){
+				
+		const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+		promises.push(fetch(url).then((res)=> res.json()));
+	}
+
+	Promise.all(promises).then( results => {
+		const pokemon = results.map((data) => ({
+			name: data.name,
+			id: data.id,
+			image: data.sprites['front_default'],
+			type: data.types.map((type) => type.type.name).join(', ')
+		}));
+		displayPokemon(pokemon);
+	});
+};
+
+const displayPokemon = (pokemon) => {
+	console.log(pokemon);
+	const pokemonHTMLString = pokemon.map (individ => `
+	<li class="card">
+	  <img class="card-image"src="${individ.image}"/>
+	  <h3 class="card-title">${individ.id}. ${individ.name}</h3>
+	  <p class="card-info">Type: ${individ.type}</p>
+	</li>
+	
+	`
+	)
+	.join('')
+	pokedex.innerHTML = pokemonHTMLString;
+
+
+};
+
+fetchPokemon();
+
+// Lägg pokemon i team
